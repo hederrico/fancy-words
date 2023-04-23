50402f57f37a0897e857b1f544bd8ef1f39da34a
Henrique Derrico <henrique.derrico@hit.com.vc>
2023-04-22 21:28:10 -0300

    Randomicidade do transform das letras

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const MAXROTATE = 45;
+const MINROTATE = -45;
+
+const MAXTRANSX = 15;
+const MINTRANSX = -15;
+
+const MAXTRANSY = 25;
+const MINTRANSY = -25;
+
 enhance = id => {
     var elem = document.getElementById(id);
     
     if(elem) {
         var word = elem.innerText;
         elem.innerHTML = '';
+        elem.addEventListener("mouseenter", rotate);
 
         for (const i in word) {
             var span = document.createElement('SPAN');
@@ -14,6 +24,18 @@ enhance = id => {
             elem.append(span);
         }
     }
+}
+
+function rotate() {
+    this.querySelectorAll('span').forEach(letter => {
+        setRandomValue(letter, '--rotateDeg', MINROTATE, MAXROTATE, 'deg');
+        setRandomValue(letter, '--transformX', MAXTRANSX, MINTRANSX, 'px');
+        setRandomValue(letter, '--transformY', MAXTRANSY, MINTRANSY, 'px');
+    });
+}
+
+function setRandomValue(elem, property, min, max, unit) {
+    elem.style.setProperty(property, (Math.floor(Math.random() * (max - min + 1)) + min) + unit);
 }
 
 enhance('github');
